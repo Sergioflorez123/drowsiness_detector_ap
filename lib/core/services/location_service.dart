ef74cf87f0354f2a4f5db9699f497ca91fc14c95
Sergioflorez123 <sergio.florezs@campusucc.edu.co>
2026-04-21 18:50:57 -0500

    agregar cambios de sensiblidad en la camara

--- lib/core/services/location_service.dart
+++ lib/core/services/location_service.dart
@@ -23,7 +23,7 @@ class LocationService {
     return Geolocator.getPositionStream(
       locationSettings: const LocationSettings(
         accuracy: LocationAccuracy.high,
-        distanceFilter: 14,
+        distanceFilter: 8,
       ),
     );
   }
