3302123f6d9944a92fd6f9f1f75aba69d6d1137f
Sergioflorez123 <sergio.florezs@campusucc.edu.co>
2026-04-22 16:16:22 -0500

    fix(home): remove “what do you want to do today” subtitle

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -136,7 +136,7 @@ class MyApp extends ConsumerWidget {
             ),
             textStyle: const TextStyle(
               fontWeight: FontWeight.w700,
-              fontSize: 28,
+              fontSize: 29,
             ),
           ),
         ),
